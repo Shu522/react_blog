493b5d7fc9dc60ce3704122d82148a20b694740c
Shu522 <aki4358@naver.com>
2024-12-02 08:58:10 +0900

    first

---
+++ src/component/Blog.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Blog = (props) => {
+  const [title, setTitle] = useState([
+    "떡볶이 맛있겠다",
+    "슈 너무 귀여워",
+    "ㅂㅂ",
+  ]);
+  const changeTitle = () => {
+    setTitle((v) =>
+      v.map((text, index) =>
+        index === 2 ? (text === "ㅂㅂ" ? "ㅎㅇ" : "ㅂㅂ") : text
+      )
+    );
+  };
+
+  const [heart, setHeart] = useState(0);
+  const up = () => {
+    setHeart((v) => v + 1);
+  };
+
+  return (
+    <>
+      <div className="bg-black p-5 text-white text-xl font-bold">Blog</div>
+      <h3 className="font-bold">
+        {title[0]} <button onClick={up}>🤍</button>{" "}
+        <span className="font-thin">{heart}</span>
+      </h3>
+      <p className="border-b">{props.date}</p>
+      <h3 className="font-bold">{title[1]}</h3>
+      <p className="border-b"> {props.date}</p>
+      <h3 className="font-bold">{title[2]}</h3>
+      <button
+        onClick={changeTitle}
+        className="text-left border border-gray-400 bg-gray-400 w-10 p-1"
+      >
+        변경
+      </button>
+      <p className="border-b">{props.date}</p>
+    </>
+  );
+};
+
+export default Blog;
